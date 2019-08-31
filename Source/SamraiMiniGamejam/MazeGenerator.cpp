90114fceecca2942e277b741fee47ad462313959
andesyv <andesyv@gmail.com>
2019-08-31 16:21:53 +0200

    Random rooms (don't work)
    For some reason whenever i change one spot in the 2d array i also change 1 or more other random spots and I can't figure out quite why...

--- Source/SamraiMiniGamejam/MazeGenerator.cpp
+++ Source/SamraiMiniGamejam/MazeGenerator.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MazeGenerator.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 AMazeGenerator::AMazeGenerator()
@@ -31,14 +31,78 @@ void AMazeGenerator::generateMaze()
 	if (maze != nullptr)
 		delete maze;
 
+	if (mazeWidth < 0)
+		mazeWidth = FMath::Abs(mazeWidth);
+	if (mazeHeight < 0)
+		mazeHeight = FMath::Abs(mazeHeight);
+
+
 	// Create array
-	maze = new unsigned int[mazeWidth * mazeHeight]{};
-	for (unsigned int i{ 0 }; i < mazeWidth * mazeHeight; ++i) {
+	maze = new char[mazeWidth * mazeHeight]{};
+	for (int i{ 0 }; i < mazeWidth * mazeHeight; ++i) {
 		// Set to 1 to indicate walls
-		maze[i] = 1;
+		maze[i] = 0b0001;
 	}
 
+	// 1. First, sprinkle in some random rooms.
+	// Make sure they don't overlap
+	int rooms = 10;
+	for (int room{ 0 }; room < rooms; ++room) {
+		int width = UKismetMathLibrary::RandomIntegerInRange(1, 7);
+		int height = UKismetMathLibrary::RandomIntegerInRange(1, 7);
 
+		int maxAttempts = 10;
+		for (int attempt = 0; attempt < maxAttempts; ++attempt) {
+			Box box{
+				width,
+				height,
+				UKismetMathLibrary::RandomInteger(mazeWidth - width),
+				UKismetMathLibrary::RandomInteger(mazeHeight - height)
+			};
+
+			// Check if ranndom box collides with any other boxes
+			if (!isOverlappingInMaze(box)) {
+				// Map out room.
+				for (int x = box.x; x < box.x + box.width; ++x) {
+					for (int y = box.y; y < box.y + box.height; ++y) {
+						maze[x * y] = 0b0000;
+					}
+				}
+				break;
+			}
+		}
+	}
+
+	/*for (int x = 0; x < mazeWidth; ++x) {
+		for (int y = 0; y < mazeHeight; ++y) {
+			
+		}
+	}*/
+}
+
+bool AMazeGenerator::isOverlapping(const Box& b1, const Box& b2)
+{
+	return
+		b1.x + b1.width / 2.f > b2.x - b2.width / 2.f &&
+		b1.x - b1.width / 2.f < b2.x + b2.width / 2.f &&
+		b1.y + b1.height / 2.f > b2.y - b2.height / 2.f &&
+		b1.y - b1.height / 2.f < b2.y + b2.height / 2.f;
+}
+
+bool AMazeGenerator::isOverlappingInMaze(const Box& b) const
+{
+	if (maze == nullptr)
+		return false;
+
+	for (int x = b.x; x < b.x + b.width; ++x) {
+		for (int y = b.y; y < b.y + b.height; ++y) {
+			if (!maze[x * y]) {
+				return true;
+			}
+		}
+	}
+
+	return false;
 }
 
 AMazeGenerator::~AMazeGenerator()
--- Source/SamraiMiniGamejam/MazeGenerator.h
+++ Source/SamraiMiniGamejam/MazeGenerator.h
@@ -12,29 +12,39 @@ class SAMRAIMINIGAMEJAM_API AMazeGenerator : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
-	AMazeGenerator();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MazeGeneration")
+		int mazeWidth = 100;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MazeGeneration")
-		unsigned int mazeWidth = 100;
+		int mazeHeight = 100;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MazeGeneration")
-		unsigned int mazeHeight = 100
-
-	~AMazeGenerator();
+	struct Box {
+		int width;
+		int height;
+		int x;
+		int y;
+	};
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 private:
-	unsigned int* maze = nullptr;
+	char* maze = nullptr;
 
 public:	
+	// Sets default values for this actor's properties
+	AMazeGenerator();
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION(BlueprintCallable)
 		void generateMaze();
+
+	static bool isOverlapping(const Box& b1, const Box& b2);
+	bool isOverlappingInMaze(const Box& b) const;
+
+	~AMazeGenerator();
 
 };
